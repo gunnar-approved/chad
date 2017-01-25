651eb23c366eed7fe559e48148589c297a8cfc3b
beardhatcode <mail.garonn@gmail.com>
2017-01-26 00:51:34 +0100

    Trying out serde json

--- src/chad/handler.rs
+++ src/chad/handler.rs
@@ -1,4 +1,8 @@
 use regex::Regex;
+use hyper::Client;
+use hyper::header::Connection;
+use std::io::Read;
+use serde_json::Value;
 
 pub trait MessageHandler {
     fn handle(&self, message: &str) -> Option<String>;
@@ -9,8 +13,13 @@ pub struct EchoHandler {
     regex: Regex,
 }
 
+pub struct WikiHandler {
+    regex: Regex,
+}
+
+
 impl MessageHandler for EchoHandler {
-    fn new(regex: Regex) -> Box<Self> {
+    fn new(regex: Regex) -> Box<MessageHandler> {
         Box::new(EchoHandler { regex: regex })
     }
 
@@ -21,4 +30,37 @@ impl MessageHandler for EchoHandler {
 
         return None;
     }
+}
+
+
+impl MessageHandler for WikiHandler {
+    fn new(regex: Regex) -> Box<MessageHandler> {
+        Box::new(WikiHandler { regex: regex })
+    }
+
+    fn handle(&self, message: &str) -> Option<String> {
+        if self.regex.is_match(message) {
+            let thing = &message[5..]; // everything after wiki, I know is not clean
+            let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&redirects=1&utf8=1&titles=".to_string()+thing;
+
+            let client = Client::new();
+            let mut res = client.get(&*url)
+                .header(Connection::close())
+                .send().unwrap();
+
+            println!("Response: {}", res.status);
+            println!("Headers:\n{}\n", res.headers);
+            let mut content = String::new();
+            res.read_to_string(&mut content);
+            println!("Resp:\n{}", content);
+
+
+
+            return Some(content.to_owned());
+        }
+
+        return None;
+    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 extern crate slack;
 extern crate regex;
+extern crate hyper;
+extern crate serde_json;
 
 mod chad;
 
 use chad::*;
 use regex::Regex;
 
+
+use serde_json::Value;
+use serde_json::Map;
+use serde_json::value::ToJson;
+
 fn main() {
     let api_key = "KEYHERE";
+    let result = r#"{"batchcomplete":"","query":{"redirects":[{"from":"Boobies","to":"Booby"}],"pages":{"223034":{"pageid":223034,"ns":0,"title":"Booby"}}}}"#;
+    let v:Value = match serde_json::from_str(result) {
+        Ok(response) => response,
+        Err(_) => panic!("So close..."),
+    };
+    let x : Map<String, Value>= v["query"]["pages"].unwrap();
+    let best = x.get(&0);
+    println!("{}", x);
+}
+
+fn main2() {
     let mut chad = Chad::new();
     init(&mut chad);
     let mut cli = slack::RtmClient::new(&api_key);
@@ -25,5 +43,8 @@ fn main() {
 fn init(chad: &mut Chad) {
     let re = Regex::new(r"echo").unwrap();
     let handler = EchoHandler::new(re);
-    chad.add_handler(handler);
+    //chad.add_handler(handler);
+    let re2 = Regex::new(r"^wiki").unwrap();
+    let handler2 = WikiHandler::new(re2);
+    chad.add_handler(handler2);
 }
